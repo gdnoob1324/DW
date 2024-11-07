2a50d84e580c2b0048770ac0f7030c2a7ce9a938
지디늅 <78996005+gdnoob1324@users.noreply.github.com>
2024-11-07 10:32:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function setupCars() {
         const newDiv = document.createElement('div');
         newDiv.classList.add('car');
         const newImg = document.createElement('img');
-        getCarImg(text).then(e => newImg.src = e == undefined ? '' : e);
+        getCarImg_old(text).then(e => newImg.src = e == undefined ? '' : e);
         newDiv.appendChild(newImg);
         const newP = document.createElement('p');
         newP.innerHTML = object[text][1] + ' <span>' + text + '</span> <span>-</span> ' + object[text][0];
