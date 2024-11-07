29c639e1368a87b3f4029fe077405e74ac5bfcf7
지디늅 <78996005+gdnoob1324@users.noreply.github.com>
2024-11-07 14:47:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ try {
     return await fetch('https://drive-world.fandom.com/api.php?action=imageserving&wisTitle=' + name + '&format=json&origin=*')
         .then(response => response.json()).then(data => {
             const text = data['image']['imageserving'];
-            return text.substring(0, text.indexOf('.png') + 4) + 'scale-to-width-down/384';
+            return text.substring(0, text.indexOf('.png') + 4) + '/scale-to-width-down/384';
         }).catch(error => { return ''; });
 } catch (error) {
         return '';
